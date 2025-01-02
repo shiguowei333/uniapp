1b8ef1c9f4151c3289682aca245bf73984124ea6
sakura3 <shiguowei333@outlook.com>
2025-01-02 22:07:21 +0800

    个人中心页面布局

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,94 @@
 <template>
-	<view>
-		我的
+	<view class="userLayout">
+		<view class="userInfo">
+			<view class="avatar">
+				<image src="/static/images/xxmLogo.png" mode="aspectFill"></image>
+			</view>
+			<view class="ip">
+				100.100.100.100
+			</view>
+			<view class="address">
+				来自于:山东
+			</view>
+		</view>
+		<view class="section">
+			<view class="list">
+				<view class="row">
+					<view class="left">
+						<uni-icons type="download-filled" size="30" color="#28b389"></uni-icons>
+						<view class="text">
+							我的下载
+						</view>
+					</view>
+					<view class="right">
+						<view class="text">
+							33
+						</view>
+						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="download-filled" size="30" color="#28b389"></uni-icons>
+						<view class="text">
+							我的下载
+						</view>
+					</view>
+					<view class="right">
+						<view class="text">
+							33
+						</view>
+						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="download-filled" size="30" color="#28b389"></uni-icons>
+						<view class="text">
+							我的下载
+						</view>
+					</view>
+					<view class="right">
+						<view class="text">
+							33
+						</view>
+						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="section">
+			<view class="list">
+				<view class="row">
+					<view class="left">
+						<uni-icons type="download-filled" size="30" color="#28b389"></uni-icons>
+						<view class="text">
+							我的下载
+						</view>
+					</view>
+					<view class="right">
+						<view class="text">
+							33
+						</view>
+						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+					</view>
+				</view>
+				<view class="row">
+					<view class="left">
+						<uni-icons type="download-filled" size="30" color="#28b389"></uni-icons>
+						<view class="text">
+							我的下载
+						</view>
+					</view>
+					<view class="right">
+						<view class="text">
+							33
+						</view>
+						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,6 +96,78 @@
 	
 </script>
 
-<style>
-	       
+<style lang="scss" scoped>
+	.userLayout {
+		.userInfo {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+			padding: 50rpx 0;
+			.avatar {
+				width: 160rpx;
+				height: 160rpx;
+				border-radius: 50%;
+				overflow: hidden;
+				image {
+					width: 100%;
+					height: 100%;
+				}
+			}
+			.ip {
+				font-size: 44rpx;
+				color: #333;
+				padding: 20rpx 0 5rpx;
+			}
+			.address {
+				font-size: 28rpx;
+				color: #aaa;
+			}
+		}
+		.section{
+				width: 690rpx;
+				margin:50rpx auto;
+				border:1px solid #eee;
+				border-radius: 10rpx;
+				box-shadow: 0 0 30rpx rgba(0,0,0,0.05);
+				.list{
+					.row{
+						display: flex;
+						justify-content: space-between;
+						align-items: center;
+						padding:0 30rpx;
+						height: 100rpx;
+						border-bottom: 1px solid #eee;
+						position: relative;
+						background: #fff;
+						&:last-child{border-bottom:0}
+						.left{
+							display: flex;
+							align-items: center;
+							.text{
+								padding-left: 20rpx;
+								color:#666
+							}
+						}
+						.right{
+							display: flex;
+							align-items: center;
+							.text{
+								font-size: 28rpx;
+								color:#aaa;
+								
+							}
+						}
+						button{
+							position: absolute;
+							top:0;
+							left:0;
+							height: 100rpx;
+							width:100%;
+							opacity: 0;
+						}
+					}
+				}
+			}
+	} 
 </style>
