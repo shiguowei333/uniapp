3c0483346ada65cb26dc197c88fd3ad6ee617ac5
sakura3 <shiguowei333@outlook.com>
2025-01-02 21:48:33 +0800

    设置项目底部tab页面切换标签

---
+++ pages/classify/classify.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="classLayout">
+		<view class="classify">
+			<theme-item v-for="item in 15"></theme-item>
+		</view>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style scoped lang="scss">
+	.classify {
+		padding: 30rpx;
+		display: grid;
+		gap: 15rpx;
+		grid-template-columns: repeat(3,1fr);
+	}
+</style>
